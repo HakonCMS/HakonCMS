3b253fac9791d047633bf05503aabd3c10e6bc7f
Leandro Sanches <leandro.sanches@efuzyon.com>
2016-05-04 22:04:26 -0300

    Changing project URL and subtitle at the welcome view

--- resources/views/core/welcome.blade.php
+++ resources/views/core/welcome.blade.php
@@ -56,8 +56,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title"><a href="https://github.com/efuzyon/hakon">Hakon</a></div>
-                <div class="subtitle">Powered by <a href="https://github.com/efuzyon" target="_blank">eFuzyon Technology</a></div>
+                <div class="title"><a href="https://github.com/HakonCMS/HakonCMS">Hakon</a></div>
+                <div class="subtitle">Created by <a href="https://github.com/efuzyon" target="_blank">eFuzyon Technology</a></div>
             </div>
         </div>
     </body>
