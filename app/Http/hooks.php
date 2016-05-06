bf8443bdd27f02f3290dcb7170ad1389f6fd2c88
Leandro Sanches <leandro.sanches@efuzyon.com>
2016-05-06 19:30:53 -0300

    Changing http/hooks to use the Hook Handler

--- app/Http/hooks.php
+++ app/Http/hooks.php
@@ -1,6 +1,7 @@
 <?php
 
 	use App\Events;
+	use App\Hooks;
 	use Collective\Html\HtmlBuilder as Html;
 
 	Html::macro("Hook", function($name = null, $param = null)
@@ -10,42 +11,10 @@
 
 			try {
 
-				Html::$name($param);
+				Event::Fire(new Hooks\Handler($name, $param));
 
 			} catch(Exception $e){}
 
 		endif;
 
-	});
-
-# Core Admin Template Menu
-	Html::macro("Core::Admin::Template::Menu::Before", function($param = null)
-	{
-		Event::Fire(new Events\EventCoreTemplateMenuBefore($param));
-	});
-
-	Html::macro("Core::Admin::Template::Menu", function($param = null)
-	{
-		Event::Fire(new Events\EventCoreTemplateMenu($param));
-	});
-
-	Html::macro("Core::Admin::Template::Menu::After", function($param = null)
-	{
-		Event::Fire(new Events\EventCoreTemplateMenuAfter($param));
-	});
-
-# Core Admin Template Menu Item
-	Html::macro("Core::Admin::Template::Menu::Item::Before", function($param = null)
-	{
-		Event::Fire(new Events\EventCoreTemplateMenuItemBefore($param));
-	});
-
-	Html::macro("Core::Admin::Template::Menu::Item", function($param = null)
-	{
-		Event::Fire(new Events\EventCoreTemplateMenuItem($param));
-	});
-
-	Html::macro("Core::Admin::Template::Menu::Item::After", function($param = null)
-	{
-		Event::Fire(new Events\EventCoreTemplateMenuItemAfter($param));
 	});
