10244531e24faae564b7ac7e614807c8e839e726
Leandro Sanches <leandro.sanches@efuzyon.com>
2016-05-06 18:15:04 -0300

    Changing hooks

--- app/Http/hooks.php
+++ app/Http/hooks.php
@@ -18,16 +18,34 @@
 
 	});
 
-	Html::macro("Core::Admin::Template::Menu", function($param = null)
+# Core Admin Template Menu
+	Html::macro("Core::Admin::Template::Menu::Before", function($param = null)
 	{
 		Event::Fire(new Events\EventCoreTemplateMenuBefore($param));
+	});
+
+	Html::macro("Core::Admin::Template::Menu", function($param = null)
+	{
 		Event::Fire(new Events\EventCoreTemplateMenu($param));
+	});
+
+	Html::macro("Core::Admin::Template::Menu::After", function($param = null)
+	{
 		Event::Fire(new Events\EventCoreTemplateMenuAfter($param));
+	});
+
+# Core Admin Template Menu Item
+	Html::macro("Core::Admin::Template::Menu::Item::Before", function($param = null)
+	{
+		Event::Fire(new Events\EventCoreTemplateMenuItemBefore($param));
 	});
 
 	Html::macro("Core::Admin::Template::Menu::Item", function($param = null)
 	{
-		Event::Fire(new Events\EventCoreTemplateMenuItemBefore($param));
 		Event::Fire(new Events\EventCoreTemplateMenuItem($param));
+	});
+
+	Html::macro("Core::Admin::Template::Menu::Item::After", function($param = null)
+	{
 		Event::Fire(new Events\EventCoreTemplateMenuItemAfter($param));
 	});
