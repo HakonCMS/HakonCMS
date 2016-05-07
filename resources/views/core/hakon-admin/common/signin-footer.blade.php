ed02b36f1d6261ce52c25a85aa7b357e1e1d7828
Leandro Sanches <leandro.sanches@efuzyon.com>
2016-05-07 16:27:14 -0300

    This closes #7 - Core views changed to adminLTE

--- resources/views/core/hakon-admin/common/signin-footer.blade.php
+++ resources/views/core/hakon-admin/common/signin-footer.blade.php
@@ -1,17 +1,17 @@
-	<!-- jQuery 2.1.4 -->
-	{!! Html::Script('core/plugins/jQuery/jQuery-2.1.4.min.js') !!}
-	<!-- Bootstrap 3.3.5 -->
-	{!! Html::Script('core/bootstrap/js/bootstrap.min.js') !!}
-	<!-- iCheck -->
-	{!! Html::Script('core/plugins/iCheck/icheck.min.js') !!}
-	<script type="text/javascript">
-		$(function () {
-			$('input').iCheck({
-				checkboxClass: 'icheckbox_square-blue',
-				radioClass: 'iradio_square-blue',
-				increaseArea: '20%' // optional
-			});
-		});
-	</script>
-	</body>
+<!-- jQuery 2.1.4 -->
+{!! Html::Script('core/plugins/jQuery/jQuery-2.1.4.min.js') !!}
+        <!-- Bootstrap 3.3.5 -->
+{!! Html::Script('core/bootstrap/js/bootstrap.min.js') !!}
+        <!-- iCheck -->
+{!! Html::Script('core/plugins/iCheck/icheck.min.js') !!}
+<script type="text/javascript">
+    $(function () {
+        $('input').iCheck({
+            checkboxClass: 'icheckbox_square-blue',
+            radioClass: 'iradio_square-blue',
+            increaseArea: '20%' // optional
+        });
+    });
+</script>
+</body>
 </html>
