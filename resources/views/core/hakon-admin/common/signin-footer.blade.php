2118ceffa316dc01da5545a028090af8c3f976af
Leandro Sanches <leandro.sanches@efuzyon.com>
2016-05-06 22:58:17 -0300

    Migrating views to foundation

--- resources/views/core/hakon-admin/common/signin-footer.blade.php
+++ resources/views/core/hakon-admin/common/signin-footer.blade.php
@@ -1,22 +1,8 @@
-    	<script>
-            var resizefunc = [];
-        </script>
-
-        <!-- jQuery  -->
-        {!! Html::Script('core/hakon/js/jquery.min.js') !!}
-        {!! Html::Script('core/hakon/js/jquery.min.js') !!}
-        {!! Html::Script('core/hakon/js/bootstrap.min.js') !!}
-        {!! Html::Script('core/hakon/js/detect.js') !!}
-        {!! Html::Script('core/hakon/js/fastclick.js') !!}
-        {!! Html::Script('core/hakon/js/jquery.slimscroll.js') !!}
-        {!! Html::Script('core/hakon/js/jquery.blockUI.js') !!}
-        {!! Html::Script('core/hakon/js/waves.js') !!}
-        {!! Html::Script('core/hakon/js/wow.min.js') !!}
-        {!! Html::Script('core/hakon/js/jquery.nicescroll.js') !!}
-        {!! Html::Script('core/hakon/js/jquery.scrollTo.min.js') !!}
-
-        {!! Html::Script('core/hakon/js/jquery.core.js') !!}
-        {!! Html::Script('core/hakon/js/jquery.app.js') !!}
-	
-	</body>
+    {!! Html::Script('core/foundation/js/vendor/jquery.js') !!}
+    {!! Html::Script('core/foundation/js/vendor/what-input.js') !!}
+    {!! Html::Script('core/foundation/js/vendor/foundation.min.js') !!}
+    <script>
+        $(document).foundation();
+    </script>
+    </body>
 </html>
